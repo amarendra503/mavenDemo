de5dcfcdd4d49b39e83fad775f712c5797cca0c3
amarendrakumar503@gmail.com <amarendrakumar503@gmail.com>
2015-08-05 17:59:02 +0530

    initial checkin of learn automation.

---
+++ com.learnautomation.selenium/src/test/java/com/learnautomation/selenium/DemoforMaven.java
@@ -0,0 +1,15 @@
+package com.learnautomation.selenium;
+
+import org.testng.annotations.Test;
+
+public class DemoforMaven {
+	@Test
+	public void testMaven() {
+		
+		
+	System.out.println("Welcome to Maven World ");
+	
+	
+	}
+
+}
